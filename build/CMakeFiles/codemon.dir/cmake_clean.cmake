671dc32eff687bbf514f54f12bcbf90052318d82
Isaac Hutchison <hutchiis@flip1.engr.oregonstate.edu>
2026-02-03 17:01:33 -0800

    Describe what you changed

---
+++ build/CMakeFiles/codemon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codemon.dir/src/main.cpp.o"
+  "CMakeFiles/codemon.dir/src/main.cpp.o.d"
+  "codemon"
+  "codemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
