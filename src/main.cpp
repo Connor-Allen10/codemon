1890df3f00d01f12426f86c400e6afa199538f7f
Connor Allen <alleconn@oregonstate.edu>
2026-02-19 09:48:02 -0800

    beta executable version 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
-#include "game/Game.h"
+#include "game/Game.hpp"
 
 int main() {
     Game game;
     game.run();
     return 0;
 }
+
+/*
+
+COMPILE COMMAND
+
+g++ main.cpp states/WorldState.cpp game/Game.cpp game/StateStack.cpp -I../include -IC:/SFML/include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -o codemontest.exe
+
+*/
