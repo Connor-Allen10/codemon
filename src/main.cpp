37db923620ee4e1ef27b7fc90e327f0c62308501
Connor Allen <alleconn@oregonstate.edu>
2026-02-19 12:14:43 -0800

    edited compile command with all cpp files

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,6 @@ int main() {
 
 COMPILE COMMAND
 
-g++ main.cpp states/WorldState.cpp game/Game.cpp game/StateStack.cpp -I../include -IC:/SFML/include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -o codemontest.exe
+g++ main.cpp battle/Battle.cpp battle/Party.cpp states/BattleState.cpp states/DebugState.cpp states/MainMenuState.cpp states/WorldState.cpp world/Map.cpp world/Obstacle.cpp world/Player.cpp game/Game.cpp game/StateStack.cpp -I../include -IC:/SFML/include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -o codemontest.exe
 
 */
